29d5cb829ee0a517d528d414c010f15abde08be4
Chris Tybur <thisiscmt@gmail.com>
2024-07-21 18:49:10 -0700

    Updated the -h command

--- Program.cs
+++ Program.cs
@@ -27,15 +27,18 @@ switch(args[0])
         break;
     case "-help":
     case "-h":
-        Console.WriteLine("Metadata Editor does various tasks involving music files and their metadata");
+        Console.WriteLine("Metadata Editor does various tasks involving music files and their metadata.");
         Console.WriteLine();
-        Console.WriteLine("Paths with spaces must be enclosed in quotes. All subdirectories within the path to the music files will be checked for the given extension. The extension parameter should not include a dot. File names are assumed to follow this standard: '<artist> - <title>'. If the output file parameter is omitted, a file will be created in the Documents directory. The following tasks are supported:");
+        Console.WriteLine("Paths with spaces must be enclosed in quotes. All subdirectories within the path to the music files will be checked for the given extension. The extension parameter should not include a dot. File names are assumed to follow this standard: '<artist> - <title>'. If the output file parameter is omitted a file will be created in the Documents directory. The following tasks are supported:");
         Console.WriteLine();
-        Console.WriteLine("Generate a list of files whose name or artist/title/album metadata value violates casing rules.");
+        Console.WriteLine("Generate a list of files whose name or artist/title/album/album artist metadata value violates casing rules.");
         Console.WriteLine("  -c [path to files] [extension] [path to output file (optional)]");
         Console.WriteLine();
-        Console.WriteLine("Generate a list of files whose name or artist/title/album metadata value contains the given text.");
+        Console.WriteLine("Generate a list of files whose name or artist/title/album/album artist metadata value contains the given text.");
         Console.WriteLine("  -s [path to files] [extension] [search text] [path to output file (optional)]");
+        Console.WriteLine();
+        Console.WriteLine("Generate a list of files with a metadata value for artist/title/album/album artist value that is either empty or non-empty.");
+        Console.WriteLine("  -f [path to files] [extension] [field name] ['empty' | 'nonempty'] [path to output file (optional)]");
 
         break;
     default:
